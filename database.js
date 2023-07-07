e3107b2697a70bdcf57508a4ee47863dc53f6e10
Santiago Varacca <santiagovaracca@gmail.com>
2023-07-07 11:35:56 -0300

    replace sequelize init

--- database.js
+++
@@ -1,6 +0,0 @@
-const { Pool } = require('pg')
-require('dotenv').config()
-
-const pool = new Pool({
-    connectionString: "postgres://default:bsE0UIfowF6O@ep-withered-salad-325579-pooler.us-east-1.postgres.vercel-storage.com:5432/verceldb?sslmode=require",
-})
--- src/db.js
+++ src/db.js
@@ -1,41 +1,30 @@
-require('dotenv').config();
-const pg = require('pg')
-const { Sequelize } = require('sequelize');
-const fs = require('fs');
-const path = require('path');
-const { POSTGRES_USER, POSTGRES_PASSWORD, POSTGRES_HOST } = process.env;
+require("dotenv").config();
+const { Sequelize } = require("sequelize");
+const fs = require("fs");
+const path = require("path");
 
-const sequelize = new Sequelize(
-   `postgres://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}/pokemon`,
-   {
-      logging: false, // set to console.log to see the raw SQL queries
-      native: false, // lets Sequelize know we can use pg-native for ~30% more speed
-      dialectModule: pg
-   }
-);
+const sequelize = new Sequelize(process.env.DATABASE_URL);
 const basename = path.basename(__filename);
 
 const modelDefiners = [];
 
 // Leemos todos los archivos de la carpeta Models, los requerimos y agregamos al arreglo modelDefiners
-fs.readdirSync(path.join(__dirname, '/models'))
-   .filter(
-      (file) =>
-         file.indexOf('.') !== 0 &&
-         file !== basename &&
-         file.slice(-3) === '.js'
-   )
-   .forEach((file) => {
-      modelDefiners.push(require(path.join(__dirname, '/models', file)));
-   });
+fs.readdirSync(path.join(__dirname, "/models"))
+  .filter(
+    (file) =>
+      file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".js"
+  )
+  .forEach((file) => {
+    modelDefiners.push(require(path.join(__dirname, "/models", file)));
+  });
 
 // Injectamos la conexion (sequelize) a todos los modelos
 modelDefiners.forEach((model) => model(sequelize));
 // Capitalizamos los nombres de los modelos ie: product => Product
 let entries = Object.entries(sequelize.models);
 let capsEntries = entries.map((entry) => [
-   entry[0][0].toUpperCase() + entry[0].slice(1),
-   entry[1],
+  entry[0][0].toUpperCase() + entry[0].slice(1),
+  entry[1],
 ]);
 sequelize.models = Object.fromEntries(capsEntries);
 
@@ -43,13 +32,13 @@ sequelize.models = Object.fromEntries(capsEntries);
 // Para relacionarlos hacemos un destructuring
 const { Pokemon, Type } = sequelize.models;
 
-Pokemon.belongsToMany(Type, { through: 'pokemon_types', timestamps: false });
-Type.belongsToMany(Pokemon, { through: 'pokemon_types', timestamps: false });
+Pokemon.belongsToMany(Type, { through: "pokemon_types", timestamps: false });
+Type.belongsToMany(Pokemon, { through: "pokemon_types", timestamps: false });
 
 // Aca vendrian las relaciones
 // Product.hasMany(Reviews);
 
 module.exports = {
-   ...sequelize.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');
-   conn: sequelize, // para importart la conexión { conn } = require('./db.js');
+  ...sequelize.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');
+  conn: sequelize, // para importart la conexión { conn } = require('./db.js');
 };
